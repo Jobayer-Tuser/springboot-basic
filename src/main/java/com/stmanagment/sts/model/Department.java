0e4d1eec330965330c04228cee589b0dff572aed
Jobayer-Tuser <Jtuser112@gmail.com>
2022-06-03 21:16:12 +0600

    using lombok plugin to make shorter our model code

--- src/main/java/com/stmanagment/sts/model/Department.java
+++ src/main/java/com/stmanagment/sts/model/Department.java
@@ -1,9 +1,18 @@
 package com.stmanagment.sts.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "departments")
 public class Department {
 
@@ -17,53 +26,4 @@ public class Department {
     private String departmentAddress;
     @Column(name = "code")
     private String departmentCode;
-
-    public Department(){}
-    public Department(Long id, String departmentName, String departmentAddress, String departmentCode){
-        this.id = id;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    public Long getId(){
-        return id;
-    }
-
-    public void setId(Long id){
-        this.id = id;
-    }
-
-    public String getDepartmentName(){
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName){
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress(){
-        return departmentAddress;
-    }
-    public void setDepartmentAddress(String departmentAddress){
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode(){
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode){
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + id +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
 }
