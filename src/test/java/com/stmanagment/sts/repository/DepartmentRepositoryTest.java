808cceaee7652c7a36648cd11afbc276816557ad
Jobayer-Tuser <Jtuser112@gmail.com>
2022-06-04 12:24:43 +0600

    written test case for department repository class

---
+++ src/test/java/com/stmanagment/sts/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,36 @@
+package com.stmanagment.sts.repository;
+
+import com.stmanagment.sts.model.Department;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class DepartmentRepositoryTest {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        Department department = Department.builder()
+                .departmentName("Chemistry")
+                .departmentAddress("Dhaka")
+                .departmentCode("CH-105")
+                .build();
+        entityManager.persist(department);
+    }
+
+    @Test
+    public void Test_findById(){
+        Department department = departmentRepository.findById(1L).get();
+        assertEquals(department.getDepartmentName(), "Chemistry");
+    }
+}
